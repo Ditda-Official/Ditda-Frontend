60de25f37584c63c6a368435e0e8cbede3673ceb
YuminPark <p353700@gmail.com>
2026-05-23 03:43:44 +0900

    #11 [REFACTOR] 아이콘-라벨 맵핑 상수로 분리

--- src/components/common/SidebarMenu.tsx
+++ src/components/common/SidebarMenu.tsx
@@ -2,29 +2,7 @@
 
 import { useState } from "react";
 
-import {
-  ExitBoldIcon,
-  ExitIcon,
-  FileBoldIcon,
-  FileIcon,
-  FileImageBoldIcon,
-  FileImageIcon,
-  ProfileCircleBoldIcon,
-  ProfileCircleIcon,
-  SearchBoldIcon,
-  SearchIcon,
-} from "@/assets/icons";
-
-type IconComponent = React.FC<React.SVGProps<SVGSVGElement>>;
-
-const ICON_MAP: Record<string, { icon: IconComponent; boldIcon: IconComponent }> = {
-  "현재 외주": { icon: FileIcon, boldIcon: FileBoldIcon },
-  "외주 찾기": { icon: SearchIcon, boldIcon: SearchBoldIcon },
-  마이페이지: { icon: ProfileCircleIcon, boldIcon: ProfileCircleBoldIcon },
-  로그아웃: { icon: ExitIcon, boldIcon: ExitBoldIcon },
-  "새 외주 작성": { icon: FileIcon, boldIcon: FileBoldIcon },
-  "진행 중 외주": { icon: FileImageIcon, boldIcon: FileImageBoldIcon },
-};
+import { SIDEBAR_ICON_MAP } from "@/constants/sidebarMenu";
 
 interface SidebarMenuProps {
   label: string;
@@ -35,7 +13,7 @@ const SidebarMenu = ({ label }: SidebarMenuProps) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const isActive = isSelected || isHovered;
-  const icons = ICON_MAP[label];
+  const icons = SIDEBAR_ICON_MAP[label];
 
   return (
     <nav
