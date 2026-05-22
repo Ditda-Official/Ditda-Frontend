26e619c0ed32ac90382e92fe0b8fa7901727e050
YuminPark <p353700@gmail.com>
2026-05-22 23:20:22 +0900

    #11 [FEAT] 헤더 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-import { ArrowDownIcon } from "@/assets/icons";
-import Button from "@/components/common/Button";
+import Header from "@/components/common/Header";
 
 const page = () => {
   return (
-    <div className="flex w-100 flex-col gap-4">
-      <Button variant="small_primary">로그인</Button>
-      <Button variant="small_secondary">로그인</Button>
-      <Button variant="small_tertiary">로그인</Button>
-      <Button variant="small_disabled">로그인</Button>
-      <Button variant="medium_primary">로그인</Button>
-      <Button variant="medium_secondary">로그인</Button>
-      <Button variant="medium_tertiary">로그인</Button>
-      <Button variant="medium_disabled">로그인</Button>
-      <Button variant="large_primary">로그인</Button>
-      <Button variant="large_disabled">로그인</Button>
-      <Button variant="certification_primary">인증하기</Button>
-      <Button variant="certification_disabled">인증하기</Button>
-      <Button variant="xsmall_primary">로그인</Button>
-      <Button variant="xsmall_primary">
-        <ArrowDownIcon />
-        로그인
-      </Button>
+    <div>
+      <Header />
     </div>
   );
 };
