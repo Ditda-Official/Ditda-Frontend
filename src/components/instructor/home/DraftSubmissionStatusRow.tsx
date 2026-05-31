080259c8e86306d1aaf675aa22cfbbc6f33a53f9
YuminPark <p353700@gmail.com>
2026-05-31 19:06:51 +0900

    #23 [FEAT] 시안 제출 현황 섹션 UI 구현

--- src/components/instructor/home/DraftSubmissionStatusRow.tsx
+++ src/components/instructor/home/DraftSubmissionStatusRow.tsx
@@ -1,10 +1,10 @@
-import { ArrowRightIcon } from "@/assets/icons";
+import { ArrowRightIcon, MatchingOffIcon, MatchingOnIcon } from "@/assets/icons";
 import Tag from "@/components/common/Tag";
 
 const DraftSubmissionStatusRow = () => {
   return (
-    <div className="flex h-15 items-center py-3">
-      <div className="flex flex-row justify-between">
+    <div className="flex h-15 cursor-pointer items-center py-3">
+      <div className="flex w-full flex-row justify-between">
         <div className="flex flex-row items-center gap-6">
           <Tag variant="default" label="D-33" />
           <p className="text-gray-70 text-body2-m w-20">교재 외지/내지</p>
@@ -13,7 +13,18 @@ const DraftSubmissionStatusRow = () => {
             <ArrowRightIcon className="text-gray-90 size-5" />
           </div>
         </div>
-        {/* 오른쪽 영역 */}
+        <div className="flex w-53 flex-row items-center justify-between py-0.5">
+          <div className="flex flex-row">
+            <MatchingOnIcon className="size-8" />
+            <MatchingOnIcon className="size-8" />
+            <MatchingOffIcon className="size-8" />
+            <MatchingOffIcon className="size-8" />
+            <MatchingOffIcon className="size-8" />
+          </div>
+          <p className="text-heading3-sb text-gray-60">
+            (<span className="text-main-main">2</span>/3)
+          </p>
+        </div>
       </div>
     </div>
   );
