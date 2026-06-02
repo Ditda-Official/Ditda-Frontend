39eeb2e9998d16deb3b0c5ea9f5468cf482c2f39
YuminPark <p353700@gmail.com>
2026-06-02 13:33:21 +0900

    #23 [REFACTOR] 새 외주 작성 수정사항 반영

--- src/app/instructor/page.tsx
+++ src/app/instructor/page.tsx
@@ -4,7 +4,7 @@ import ModifyingCommissionsSection from "@/containers/instructor/home/ModifyingC
 
 const page = () => {
   return (
-    <div className="mx-auto my-auto flex w-275 flex-col items-center gap-10">
+    <div className="mx-auto flex w-275 flex-col items-center gap-10 pt-10 pb-14">
       <h1 className="text-gray-90 text-heading1-sb w-full text-left">다현님, 어서오세요!</h1>
       <DraftSubmissionStatusSection />
       <div className="flex w-full flex-row gap-5">
