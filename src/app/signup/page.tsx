30d617fab4bb59a958be03255a299c0f1020d756
KwonOjin <ojin630@naver.com>
2026-06-18 01:58:40 +0900

    #33 [FEAT] 회원가입 위젯 추가 및 단일 URL 적용

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
-import { type SignupRole, UserTypeStep } from "@/features/signup";
+import { SignupFunnel } from "@/widgets/signup";
 
 const Page = () => {
-  const router = useRouter();
-
-  const handleNext = (selectedType: SignupRole) => {
-    router.push(`/signup/${selectedType}/step1`);
-  };
-
-  return <UserTypeStep onNext={handleNext} />;
+  return <SignupFunnel />;
 };
 
 export default Page;
