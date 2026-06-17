4414a0b61d2682d0694faae167a1dee3c75a875b
KwonOjin <ojin630@naver.com>
2026-06-18 01:40:30 +0900

    #33 [REFACTOR] 회원가입 공통 타입 및 퍼널 config 정리

--- src/features/signup/index.ts
+++ src/features/signup/index.ts
@@ -1,4 +1,6 @@
 export * from "./config/signup";
+export * from "./config/signupFunnel";
+export * from "./model/signup";
 export { useSignupStep2Form } from "./model/useSignupStep2Form";
 export { default as BankDropdown } from "./ui/BankDropdown";
 export { default as UserTypeBtn } from "./ui/UserTypeBtn";
