0da7fe89adf2425bace68e6b9d25875bfd88262c
KwonOjin <ojin630@naver.com>
2026-07-01 02:02:50 +0900

    #42 [FEAT] 디자이너 외주찾기 페이지 퍼블리싱

---
+++ src/features/designer/search/ui/MatchingWaitingHeader.tsx
@@ -0,0 +1,18 @@
+const MatchingWaitingHeader = () => {
+  return (
+    <div className="border-b-gray-40 border-b px-6 pb-3">
+      <div className="flex w-fit items-start gap-29">
+        <div className="flex w-121.25 shrink-0 items-center gap-7">
+          <p className="w-11">시간</p>
+          <p className="w-103.25">외주</p>
+        </div>
+        <div className="flex w-63 shrink-0 items-center gap-20">
+          <p className="w-18">기본금</p>
+          <p className="w-25">최대 수령액</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MatchingWaitingHeader;
