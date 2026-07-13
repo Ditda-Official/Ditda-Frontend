aae403bdaac68ddc00f2be44140654dba8c534e6
KwonOjin <ojin630@naver.com>
2026-07-14 02:19:56 +0900

    #63 [FEAT] 디자이너 마이 페이지 통계 조회 API 연동

--- src/features/designer/my/index.ts
+++ src/features/designer/my/index.ts
@@ -1,4 +1,4 @@
-export type { PaymentHistory } from "./api/myTypes";
-export { AMOUNT_TYPE_DISPLAY_MAP, CATEGORY_BADGE_MAP } from "./api/myTypes";
+export type { DesignerLevel, MyInfo, PaymentHistory } from "./api/myTypes";
+export { AMOUNT_TYPE_DISPLAY_MAP, CATEGORY_BADGE_MAP, LEVEL_DISPLAY_MAP } from "./api/myTypes";
 export { default as PaymentHistoryHeader } from "./ui/PaymentHistoryHeader";
 export { default as PaymentHistoryRow } from "./ui/PaymentHistoryRow";
